9f871efa9844218cca9d34a2275dce30dd98ad6c
邹景立 <zoujingli@qq.com>
2023-02-28 22:36:50 +0800

    去除余额管理

--- src/Service.php
+++ src/Service.php
@@ -19,7 +19,6 @@ declare (strict_types=1);
 namespace plugin\account;
 
 use plugin\account\model\PluginAccountUserAddress;
-use plugin\account\model\PluginAccountUserBalance;
 use think\admin\Plugin;
 
 class Service extends Plugin
@@ -30,22 +29,14 @@ class Service extends Plugin
     {
         // 主账号绑定处理
         $this->app->event->listen('ThinkPlugsAccountBind', function (array $data) {
-            // 更新数据条件
             $map = [['unid', '<>', $data['unid']], ['usid', '=', $data['usid']]];
-            // 更新收货地址
             PluginAccountUserAddress::mk()->where($map)->update(['unid' => $data['unid']]);
-            // 更新余额记录
-            PluginAccountUserBalance::mk()->where($map)->update(['unid' => $data['unid']]);
         });
 
         // 主账号解绑处理
         $this->app->event->listen('ThinkPlugsAccountUnbind', function (array $data) {
-            // 更新数据条件
             $map = [['unid', '>', 0], ['usid', '=', $data['usid']]];
-            // 更新收货地址
             PluginAccountUserAddress::mk()->where($map)->update(['usid' => 0]);
-            // 更新余额记录
-            PluginAccountUserBalance::mk()->where($map)->update(['usid' => 0]);
         });
     }
 
@@ -58,7 +49,6 @@ class Service extends Plugin
                 'subs' => [
                     ['name' => '用户账号管理', 'icon' => 'layui-icon layui-icon-user', 'node' => "{$name}/master/index"],
                     ['name' => '用户终端管理', 'icon' => 'layui-icon layui-icon-cellphone', 'node' => "{$name}/device/index"],
-                    ['name' => '用户余额管理', 'icon' => 'layui-icon layui-icon-rmb', 'node' => "{$name}/balance/index"],
                 ],
             ],
         ];
--- stc/database/20230224000001_install_account.php
+++ stc/database/20230224000001_install_account.php
@@ -31,7 +31,6 @@ class InstallAccount extends Migrator
         $this->_create_plugin_account_bind();
         $this->_create_plugin_account_user();
         $this->_create_plugin_account_user_address();
-        $this->_create_plugin_account_user_balance();
     }
 
     /**
@@ -198,50 +197,6 @@ class InstallAccount extends Migrator
             ->addIndex('unid', ['name' => 'idx_plugin_account_user_address_unid'])
             ->addIndex('phone', ['name' => 'idx_plugin_account_user_address_phone'])
             ->addIndex('deleted', ['name' => 'idx_plugin_account_user_address_deleted'])
-            ->save();
-
-        // 修改主键长度
-        $this->table($table)->changeColumn('id', 'integer', ['limit' => 20, 'identity' => true]);
-    }
-
-    /**
-     * 创建数据对象
-     * @class PluginAccountUserBalance
-     * @table plugin_account_user_balance
-     * @return void
-     */
-    private function _create_plugin_account_user_balance()
-    {
-
-        // 当前数据表
-        $table = 'plugin_account_user_balance';
-
-        // 存在则跳过
-        if ($this->hasTable($table)) return;
-
-        // 创建数据表
-        $this->table($table, [
-            'engine' => 'InnoDB', 'collation' => 'utf8mb4_general_ci', 'comment' => '插件-账号-余额',
-        ])
-            ->addColumn('unid', 'integer', ['limit' => 20, 'default' => 0, 'null' => true, 'comment' => '账号编号'])
-            ->addColumn('code', 'string', ['limit' => 20, 'default' => '', 'null' => true, 'comment' => '操作编号'])
-            ->addColumn('name', 'string', ['limit' => 200, 'default' => '', 'null' => true, 'comment' => '操作名称'])
-            ->addColumn('remark', 'string', ['limit' => 999, 'default' => '', 'null' => true, 'comment' => '操作备注'])
-            ->addColumn('amount', 'decimal', ['precision' => 20, 'scale' => 2, 'default' => '0.00', 'null' => true, 'comment' => '操作金额'])
-            ->addColumn('cancel', 'integer', ['limit' => 1, 'default' => 0, 'null' => true, 'comment' => '作废状态(0未作废,1已作废)'])
-            ->addColumn('unlock', 'integer', ['limit' => 1, 'default' => 0, 'null' => true, 'comment' => '解锁状态(0锁定中,1已生效)'])
-            ->addColumn('deleted', 'integer', ['limit' => 1, 'default' => 0, 'null' => true, 'comment' => '删除状态(0未删除,1已删除)'])
-            ->addColumn('create_by', 'integer', ['limit' => 20, 'default' => 0, 'null' => true, 'comment' => '系统用户'])
-            ->addColumn('cancel_time', 'datetime', ['default' => NULL, 'null' => true, 'comment' => '作废时间'])
-            ->addColumn('unlock_time', 'datetime', ['default' => NULL, 'null' => true, 'comment' => '解锁时间'])
-            ->addColumn('create_time', 'datetime', ['default' => NULL, 'null' => true, 'comment' => '创建时间'])
-            ->addColumn('update_time', 'datetime', ['default' => NULL, 'null' => true, 'comment' => '更新时间'])
-            ->addIndex('unid', ['name' => 'idx_plugin_account_user_balance_unid'])
-            ->addIndex('code', ['name' => 'idx_plugin_account_user_balance_code'])
-            ->addIndex('cancel', ['name' => 'idx_plugin_account_user_balance_cancel'])
-            ->addIndex('unlock', ['name' => 'idx_plugin_account_user_balance_unlock'])
-            ->addIndex('deleted', ['name' => 'idx_plugin_account_user_balance_deleted'])
-            ->addIndex('create_time', ['name' => 'idx_plugin_account_user_balance_create_time'])
             ->save();
 
         // 修改主键长度
